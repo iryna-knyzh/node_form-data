9dc5a408e61a4c51ee3b6b16a80880b958837e47
Iryna Knyzh <iryna.knyzh@gmail.com>
2026-02-18 12:43:31 +0200

    task

--- src/createServer.js
+++ src/createServer.js
@@ -1,8 +1,51 @@
 'use strict';
 
+const http = require('node:http');
+const fs = require('node:fs');
+const path = require('path');
+
 function createServer() {
-  /* Write your code here */
-  // Return instance of http.Server class
+  const server = http.createServer((req, res) => {
+    if (req.method !== 'POST' || req.url !== '/add-expense') {
+      res.statusCode = 404;
+      res.end('Not Found');
+
+      return;
+    }
+
+    const chunks = [];
+
+    req.on('data', (chunk) => {
+      chunks.push(chunk);
+    });
+
+    req.on('end', () => {
+      const body = Buffer.concat(chunks).toString('utf-8');
+
+      const expense = JSON.parse(body);
+
+      if (!expense.date || !expense.title || !expense.amount) {
+        res.statusCode = 400;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Missing required fields');
+
+        return;
+      }
+
+      const dataPath = path.resolve(__dirname, '../db/expense.json');
+
+      fs.writeFile(dataPath, body, (error) => {
+        if (error) {
+        }
+      });
+
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.end(JSON.stringify(expense));
+    });
+  });
+
+  return server;
 }
 
 module.exports = {
